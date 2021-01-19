859a3e2c1493b761362659f4d93f6f36907df33e
Galais Valere <valeregbv@gmail.com>
2021-01-19 09:48:38 +0100

    Version corrigé des 6 exos et ajout nécessaire

--- src/worldOfJava/Monstre.java
+++ src/worldOfJava/Monstre.java
@@ -8,51 +8,21 @@ public class Monstre extends AbstractCombattant {
 
 	/**
 	 * Mise en place du constructeur
-	 * @param pointDeVie
-	 * @param degat
 	 * @param nom
+	 * @param degat
+	 * @param pointDeVie
 	 * @param QspellCd
 	 */
-	public Monstre (int pointDeVie, int degat, String nom, int QspellCd) {
-		super(nom, pointDeVie, degat, QspellCd);
+	public Monstre (String nom, int degat, int pointDeVie) {
+		super(nom, pointDeVie, degat);
 	}
-
-	/**
-	 * Mise en place des getters et setters
-	 */
-	public int getPointDeVie() {
-		return pointDeVie;
-	}
-	public void setPointDeVie(int pointDeVie) {
-		this.pointDeVie = pointDeVie;
-	}
-
-	public int getDegat() {
-		return degat;
-	}
-	public void setDegat(int degat) {
-		this.degat = degat;
-	}
-
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public int getQspellCd() {
-		return QspellCd;
-	}
-	public void setQspellCd(int qspellCd) {
-		QspellCd = qspellCd;
-	}
-
+	
 	@Override
 	public String toString() {
-		return "Le monstre " + this.nom + " à " + this.pointDeVie + " points de vie et une moyenne de " + this.degat + " dégats de base,"
-				+ " de plus le cd de son sort Q est de  " + this.QspellCd + " secondes. \n";
+		return "Le monstre " + this.nom + " à " + this.pointDeVie + " points de vie et une moyenne de " + this.degat + " dégats de base. \n";
 	}
+
+
 
 
 }
--- src/worldOfJava/Personnage.java
+++ src/worldOfJava/Personnage.java
@@ -14,48 +14,18 @@ public class Personnage extends AbstractCombattant {
 	 * @param nom
 	 * @param QspellCd
 	 */
-	public Personnage(int pointDeVie, int degat, String nom, int QspellCd) {
-		super(nom , pointDeVie, degat, QspellCd);
-	}
-
-	/**
-	 * Mise en place des getters et setters
-	 */
-	public int getPointDeVie() {
-		return pointDeVie;
-	}
-	public void setPointDeVie(int pointDeVie) {
-		this.pointDeVie = pointDeVie;
-	}
-
-	public int getDegat() {
-		return degat;
-	}
-	public void setDegat(int degat) {
-		this.degat = degat;
-	}
-
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public int getQspellCd() {
-		return QspellCd;
-	}
-	public void setQspellCd(int qspellCd) {
-		QspellCd = qspellCd;
+	public Personnage(int pointDeVie, int degat, String nom) {
+		super(nom , pointDeVie, degat);
 	}
 	/**
 	 * Affichage du personnage
 	 */
 	@Override
 	public String toString() {
-		return "Le personnage " + this.nom + " à " + this.pointDeVie + " points de vie et une moyenne de " + this.degat + " dégats de base,"
-				+ " de plus le cd de son sort Q est de  " + this.QspellCd + " secondes. Bonne chance. \n ";
+		return "Le personnage " + this.nom + " à " + this.pointDeVie + " points de vie et une moyenne de " + this.degat + " dégats de base. Bonne chance. \n ";
 	}
+
+	
 
 
 
